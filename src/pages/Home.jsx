d9d4be5aa26d4bbc277050cc52caffcf9c90a8cf
Mhmd-irsyd <irsyadmuhammad4321@gmail.com>
2025-05-02 14:42:24 +0700

    fix overflow hidden

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,34 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
+
 const Home = () => {
   return (
     <div className="overflow-x-hidden">
+      {/* Navbar */}
       <section>
         <Navbar />
       </section>
 
-      <section>
+      {/* Hero Section */}
+      <section id="hero">
         <Hero />
       </section>
 
-      <section>
+      {/* About Section */}
+      <section id="about">
         <About />
       </section>
 
-      <section>
+      {/* Projects Section */}
+      <section id="projects">
         <Projects />
       </section>
 
-      <section>
+      {/* Contact Section */}
+      <section id="contact">
         <Contact />
       </section>
     </div>
