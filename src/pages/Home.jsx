6c51ed9b4ce7a83e372fafca53bf5ca3abc293be
Mhmd-irsyd <irsyadmuhammad4321@gmail.com>
2025-05-02 14:37:14 +0700

    fix overflow hidden di home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,6 @@
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import About from "../components/About";
-import Projects from "../components/Projects";
-import Contact from "../components/Contact"; // ✅ Tambahkan import Contact
-
 const Home = () => {
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <section>
         <Navbar />
       </section>
@@ -24,7 +18,7 @@ const Home = () => {
       </section>
 
       <section>
-        <Contact /> {/* ✅ Tambahkan section contact di sini */}
+        <Contact />
       </section>
     </div>
   );
