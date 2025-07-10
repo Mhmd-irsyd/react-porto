cce571ff06385c2f206dc62db8ee953e70efb6fa
Mhmd-irsyd <irsyadmuhammad4321@gmail.com>
2025-07-10 23:59:01 +0700

    update vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'), // opsional, buat alias jika mau pakai @/components dst.
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    open: true,
+  },
+  // ⬇⬇ Tambahkan ini untuk mencegah error 404 saat refresh di halaman lain
+  base: '/',
+  // ⬇⬇ Vite pakai plugin bawaan untuk SPA fallback
+  // Tapi kalau kamu hosting di luar (seperti Vercel), pastikan Vercel juga diatur pakai `vercel.json`
 })
